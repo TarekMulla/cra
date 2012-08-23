413a1b14511e4e20c51062cfd280308dd6eef1bf
paulbeltran <paulbeltran@gmail.com>
2012-08-23 12:54:56 +0000

    se agrega sp faltante

--- AlterTableInserts/Release 1.5.1.10.sql
+++ AlterTableInserts/Release 1.5.1.10.sql
@@ -248,3 +248,12 @@ WHERE Id = @IdAsignacion
 
 
 go
+
+alter PROCEDURE [dbo].[SP_C_PAPERLESS_NUMCONSOLIDADO]  
+@numConsolidado nvarchar (100)                                  
+   
+AS                                                                                                                        
+   
+select NumConsolidado  from  PAPERLESS_USUARIO1_HOUSESBL_INFO where NumConsolidado = @numConsolidado 
+
+go

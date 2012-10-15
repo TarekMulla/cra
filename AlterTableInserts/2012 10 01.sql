f2aafd47297522ac08cc1882d173b393994adb42
paulbeltran <paulbeltran@gmail.com>
2012-10-15 19:42:51 +0000

    actualizacion de valores

--- AlterTableInserts/2012 10 01.sql
+++ AlterTableInserts/2012 10 01.sql
@@ -1,11 +1,5 @@
-/****** Object:  StoredProcedure [dbo].[SP_U_PARAM_COMUNA]    Script Date: 09/29/2012 16:46:50 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
-
-ALTER procedure [dbo].[SP_U_PARAM_COMUNA] 
+CREATE procedure [dbo].[SP_U_PARAM_COMUNA] 
 @IdComuna int,
 @IdRegion int,
 @Descripcion varchar (100)
@@ -22,19 +16,9 @@ where id=@IdComuna
 --idCiudad es la region
 --descripcion nombre de la comuna
 
-
-
-
-
-
-/****** Object:  StoredProcedure [dbo].[SP_N_PARAM_COMUNA]    Script Date: 09/29/2012 16:05:38 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
+go
 ALTER procedure [dbo].[SP_N_PARAM_COMUNA]
-@IdCiudad int,
+ @IdCiudad int,
 @Descripcion varchar (100)
 as
 
@@ -47,51 +31,61 @@ if (@Existe is null)
 	
 SELECT SCOPE_IDENTITY()
 
+go
+
+create  PROCEDURE [dbo].[SP_N_PAPERLESS_NAVIERAV2]  
+@Descripcion nvarchar(1000),  
+@Activo bit   
+  
+AS  
+  
+declare @Existe int  
+set @Existe = 0  
+select @Existe = (select top 1 id from PAPERLESS_NAVIERA  where descripcion =  rtrim(ltrim (@Descripcion))) if (@Existe is null)  begin   INSERT INTO PAPERLESS_NAVIERA(Descripcion,Activo,FechaCreacion)  
+  VALUES(rtrim(ltrim(@Descripcion)),@Activo,GETDATE())  end  
+  
+SELECT SCOPE_IDENTITY()  
 
 
+go
 
 
-/****** Object:  StoredProcedure [dbo].[SP_N_PAPERLESS_NAVIERA]    Script Date: 09/13/2012 16:45:38 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 ALTER PROCEDURE [dbo].[SP_N_PAPERLESS_NAVIERA]
 @Descripcion nvarchar(1000),
-@Activo bit,
-@Existe int 
+@Activo bit
 
 AS
 
-select @Existe = (select 1 from PAPERLESS_NAVIERA  where descripcion =  rtrim(ltrim (@Descripcion)))
-if (@Existe != 1)
-	begin
-		INSERT INTO PAPERLESS_NAVIERA(Descripcion,Activo,FechaCreacion)
-		VALUES(rtrim(ltrim(@Descripcion)),@Activo,GETDATE())
-	end
+INSERT INTO PAPERLESS_NAVIERA(Descripcion,Activo,FechaCreacion)
+VALUES(@Descripcion,@Activo,GETDATE())
+
 
 SELECT SCOPE_IDENTITY()
+go
+
+
+create procedure [dbo].[SP_U_PAPERLESS_NAVIERA] 
+
+@Id int, 
+@Descripcion varchar (100)  
+as  
+
+update PAPERLESS_NAVIERA set Descripcion = rtrim(ltrim(@Descripcion))  where id=@Id
+
+go
+
+
+create PROCEDURE [dbo].[SP_L_PAPERLESS_NAVIERAALL]  
+
+  
+AS  
+  
+SELECT Id, Descripcion, Activo ,FechaCreacion FROM PAPERLESS_NAVIERA    
+
+go
 
 
 
-
-create  procedure SP_U_PAPERLESS_NAVIERA
-@Id int,
-@Descripcion varchar (100)
-
-as
-
-update PAPERLESS_NAVIERA set Descripcion = rtrim(ltrim(@Descripcion)) 
-where id=@Id
-
-
-
-
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 ALTER procedure  [dbo].[SP_N_CLIENTES_CLIENTE_MASTER]
 @NombreCompania nvarchar(255),
 @Nombres nvarchar(255),
@@ -105,28 +99,34 @@ ALTER procedure  [dbo].[SP_N_CLIENTES_CLIENTE_MASTER]
 @IdUsuario int
 AS
 
+
 if @DireccionInfo = -1 set @DireccionInfo = NULL
 
+
 DECLARE @EXISTE INT
+
 --SELECT @EXISTE = (SELECT top 1 1 FROM CLIENTES_MASTER WHERE RUT = @RUT AND CODTIPO = @CODTIPO)
-SELECT @EXISTE = (SELECT top 1 1 FROM CLIENTES_MASTER WHERE replace (RUT,' ','') = replace (@RUT,' ','') AND CodTipo = ISNULL(@CodTipo,CodTipo))
+IF (@CodTipo in (1,2))
+Begin
+	if (replace (@RUT,' ','') is not null)
+	BEGIN
+	SELECT @EXISTE = (SELECT top 1 1 FROM CLIENTES_MASTER WHERE replace (RUT,' ','') = replace (@RUT,' ','') AND CodTipo = ISNULL(@CodTipo,CodTipo))
+	END
+End 
+
 --exec SP_C_CLIENTE_MASTER_EXISTE_RUT @RUT,@CODTIPO ,@EXISTE output
 IF (@EXISTE IS NULL)
 BEGIN
-INSERT INTO CLIENTES_MASTER (NombreCompania,Nombres,ApellidoPaterno,ApellidoMaterno, RUT,CodTipo,FechaCreacion,IdDireccionInfo,NombreFantasia,IdUsuario)
+	INSERT INTO CLIENTES_MASTER (NombreCompania,Nombres,ApellidoPaterno,ApellidoMaterno, RUT,CodTipo,FechaCreacion,IdDireccionInfo,NombreFantasia,IdUsuario)
 VALUES (@NombreCompania,@Nombres,@ApPaterno,@ApMaterno, @RUT,@CodTipo,GetDate(),@DireccionInfo,@NombreFantasia,@IdUsuario)
 
 SELECT  SCOPE_IDENTITY()
 END
 
+go
 
 
--- insert into PERFILES values ('Administrador de Datos Maestros',GETDATE(),null)
---insert into USUARIOS_PERFILES values (43,21,0)
 
-ALTER PROCEDURE [dbo].[SP_L_PAPERLESS_NAVIERAALL]  
-
-  
-AS  
-  
-SELECT Id, Descripcion, Activo ,FechaCreacion FROM PAPERLESS_NAVIERA    
+insert into PERFILES values ('AdministradorDatosMaestros',GETDATE(),null)
+insert into USUARIOS_PERFILES values (44,22,0)-- asignacion de datos maestros pbeltran
+go

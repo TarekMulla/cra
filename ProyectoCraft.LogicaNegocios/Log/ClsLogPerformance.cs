851ad29df1a28a7b697ce1be21d3b4afc78c6424
vhspiceros <vhspiceros@c3772aea-f7fb-1ae5-96b0-6ad07730f538>
2012-10-19 00:22:01 +0000

    se mejora el metodo Save del log de performance

--- ProyectoCraft.LogicaNegocios/Log/ClsLogPerformance.cs
+++ ProyectoCraft.LogicaNegocios/Log/ClsLogPerformance.cs
@@ -1,12 +1,15 @@
-﻿using ProyectoCraft.AccesoDatos.LogPerfomance;
+﻿using System;
+using ProyectoCraft.AccesoDatos.LogPerfomance;
 using ProyectoCraft.Entidades.Log;
 
 namespace ProyectoCraft.LogicaNegocios.Log {
     public static class ClsLogPerformance {
         public static void Save(LogPerformance logPerformance) {
             var st = new System.Diagnostics.StackTrace();
-            logPerformance.Accion = st.GetFrame(1).GetMethod().Name;
-            logPerformance.Modulo = st.GetFrame(1).GetMethod().ReflectedType.FullName;
+            if (String.IsNullOrEmpty(logPerformance.Accion))
+                logPerformance.Accion = st.GetFrame(1).GetMethod().Name;
+            if (String.IsNullOrEmpty(logPerformance.Modulo))
+                logPerformance.Modulo = st.GetFrame(1).GetMethod().ReflectedType.FullName;
             ClsLogPerformanceADO.Save(logPerformance);
         }
     }

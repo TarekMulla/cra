95944a8d2d65e6805c18163c341e80863efc2683
paulbeltran <paulbeltran@c3772aea-f7fb-1ae5-96b0-6ad07730f538>
2013-05-03 21:47:14 +0000

    Carga perfiles colombia

--- AlterTableInserts/Limpieza SCC/carga inicial colombia.sql
+++ AlterTableInserts/Limpieza SCC/carga inicial colombia.sql
@@ -17,3 +17,15 @@ insert into VENTAS_TIPOS_TRAFICO values
 insert into VENTAS_TIPOS_TRAFICO values
 ('Asia - Buenaventura',GETDATE(),'2063-04-08 10:01:39.907')
 go
+
+insert into usuarios values ('Adriana','Cardenas','','acardenas',1,GETDATE(),5,'acardenas@craft-colgrupajes.com.co',3,3)
+insert into usuarios values ('Erika','Martinez','','emartinez',1,GETDATE(),1,'emartinez@craft-colgrupajes.com.co',3,3)
+insert into usuarios values ('Jenny','Munar','','ope.asia',1,GETDATE(),19,'ope.asia@craft-colgrupajes.com.co',3,3)
+insert into usuarios values ('Oscar','Zamora','','mzamora',1,GETDATE(),18,'mzamora@craft-colgrupajes.com.co',3,3)
+insert into usuarios values ('Aura','Gonzalez','','agonzalez',1,GETDATE(),4,'agonzalez@craft-colgrupajes.com.co',3,3)
+
+insert into  USUARIOS_PERFILES values (65,5,0)
+insert into  USUARIOS_PERFILES values (66,1,0)
+insert into  USUARIOS_PERFILES values (67,19,0)
+insert into  USUARIOS_PERFILES values (68,18,0)
+insert into  USUARIOS_PERFILES values (69,4,0)

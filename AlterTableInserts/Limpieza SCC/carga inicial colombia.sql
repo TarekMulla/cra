1411b62cccffd33a38de6c9fa30dc4b0f0b3930b
paulbeltran <paulbeltran@c3772aea-f7fb-1ae5-96b0-6ad07730f538>
2013-05-07 05:17:11 +0000

    update por carga de usuarios

--- AlterTableInserts/Limpieza SCC/carga inicial colombia.sql
+++ AlterTableInserts/Limpieza SCC/carga inicial colombia.sql
@@ -32,3 +32,63 @@ insert into  USUARIOS_PERFILES values (69,4,0)
 
 
 update USUARIOS set Estado = 0 where Id not in (65,66,67,68,69)
+
+
+insert into usuarios values ('Carolina','Delgado','','cdelgado',1,GETDATE(),16,'servicioalcliente@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (70,16,0)
+insert into  USUARIOS_PERFILES values (67,15,0)
+go
+insert into usuarios values ('Carolina','Amaya','','camaya',1,GETDATE(),17,'operation1.colombia@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (71,17,0)
+go
+insert into usuarios values ('Yuliana','Calderon','','ycalderon',1,GETDATE(),16,'servicioalcliente1@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (72,17,0)
+go
+insert into usuarios values ('Angie','Rodriguez','','arodriguez',1,GETDATE(),16,'arodriguez@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (73,17,0)
+go
+insert into usuarios values ('Katherine','Camargo','','k.camargo',1,GETDATE(),16,'k.camargo@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (74,17,0)
+go
+insert into usuarios values ('Jose','Castrillon','','jcastrillon',1,GETDATE(),2,'jcastrillon@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (75,2,0)
+go
+
+insert into usuarios values ('Lorena','Amya','','aamaya',1,GETDATE(),2,'aamaya@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (76,2,0)
+go
+
+insert into usuarios values ('Marcela','Urueña','','muruena',1,GETDATE(),2,'muruena@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (77,2,0)
+go
+
+insert into usuarios values ('Silvia','Triana','','striana',1,GETDATE(),2,'striana@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (78,2,0)
+go
+
+insert into usuarios values ('Lina','Ochoa','','lochoa',1,GETDATE(),2,'lochoa@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (79,2,0)
+go
+insert into usuarios values ('Marcela','Suarez','','msuarez',1,GETDATE(),2,'msuarez@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (80,2,0)
+go
+
+
+insert into usuarios values ('Johana','Villamor','','jvillamor',1,GETDATE(),1,'jvillamor@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (81,1,0)
+go
+insert into usuarios values ('Liliana','Morales','','lmorales',1,GETDATE(),1,'lmorales@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (82,1,0)
+go
+
+insert into usuarios values ('Monica','Olaya','','ma.olaya',1,GETDATE(),1,'ma.olaya@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (83,1,0)
+go
+
+insert into usuarios values ('Osman','Palacios','','opalacios',1,GETDATE(),1,'opalacios@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (84,1,0)
+go
+
+insert into usuarios values ('Delia','Porras','','dporras',1,GETDATE(),1,'dporras@craft-colgrupajes.com.co',3,3)
+insert into  USUARIOS_PERFILES values (85,1,0)
+go

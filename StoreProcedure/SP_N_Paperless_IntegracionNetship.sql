8e74873a9dd297dea1d57b344b9c746d2890dc4e
paulbeltran <paulbeltran@c3772aea-f7fb-1ae5-96b0-6ad07730f538>
2013-07-11 04:37:52 +0000

    tabla y sp de log

---
+++ StoreProcedure/SP_N_Paperless_IntegracionNetship.sql
@@ -0,0 +1,39 @@
+drop table Paperless_IntegracionNetship
+go
+Create table Paperless_IntegracionNetship
+(
+IDPaperless bigint not null primary key,
+ValorPaperless varchar (20),
+ValorNetShip varchar (20),
+Mensaje varchar (255),
+CreateDate datetime
+)
+go
+
+Create procedure SP_N_Paperless_IntegracionNetship
+
+@IDPaperless bigint ,
+@ValorPaperless varchar (20),
+@ValorNetShip varchar (20),
+@Mensaje varchar (255)
+
+AS
+
+
+INSERT INTO Paperless_IntegracionNetship
+           (IDPaperless ,
+			ValorPaperless ,
+			ValorNetShip ,
+			Mensaje ,
+			CreateDate )
+
+VALUES
+           (@IDPaperless ,
+			@ValorPaperless ,
+			@ValorNetShip ,
+			@Mensaje,
+			GetDate())
+			
+SELECT  SCOPE_IDENTITY()
+
+go

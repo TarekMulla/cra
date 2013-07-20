030a4e55edb5ee79e31f66c9c605e767b6de2a8f
paulbeltran <paulbeltran@c3772aea-f7fb-1ae5-96b0-6ad07730f538>
2013-07-20 01:54:30 +0000

    cambio en la creacion de la tabla

--- StoreProcedure/SP_N_Paperless_IntegracionNetship.sql
+++ StoreProcedure/SP_N_Paperless_IntegracionNetship.sql
@@ -1,14 +1,19 @@
 drop table Paperless_IntegracionNetship
 go
-Create table Paperless_IntegracionNetship
+CREATE TABLE [dbo].[PAPERLESS_INTEGRACIONNETSHIP](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[IDPaperless] [bigint] NOT NULL,
+	[ValorPaperless] [varchar](20) NULL,
+	[ValorNetShip] [varchar](20) NULL,
+	[Mensaje] [varchar](255) NULL,
+	[CreateDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
 (
-IDPaperless bigint not null primary key,
-ValorPaperless varchar (20),
-ValorNetShip varchar (20),
-Mensaje varchar (255),
-CreateDate datetime
-)
-go
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
 
 Create procedure SP_N_Paperless_IntegracionNetship
 

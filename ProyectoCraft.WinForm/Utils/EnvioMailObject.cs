eb4a322ca2f9c092b447758bcf5290562e75fe9a
paulbeltran <paulbeltran@c3772aea-f7fb-1ae5-96b0-6ad07730f538>
2013-08-26 01:22:34 +0000

    correccion de envio de mail

--- ProyectoCraft.WinForm/Utils/EnvioMailObject.cs
+++ ProyectoCraft.WinForm/Utils/EnvioMailObject.cs
@@ -1052,20 +1052,7 @@ namespace SCCMultimodal.Utils {
                 sb.Replace("[USUARIOCONTRAPARTE]", asignacion.Usuario1.NombreCompleto);
                 sb.Replace("[OBSERVACION]", asignacion.ObservacionUsuario1);
                 sb.Replace("[SALTO]", "\n");
-                EmailBody = sb.ToString();
-                try
-                {
-                    string MailEnCopia = System.Configuration.ConfigurationSettings.AppSettings.Get("MailEnCopia");
-                    if (!string.IsNullOrEmpty(MailEnCopia))
-                        if (!asignacion.Usuario2.Email.Contains(MailEnCopia.Replace(";", "")))
-                        {
-                            asignacion.Usuario2.Email += MailEnCopia;
-                        }
-                }
-                catch (Exception ex)
-                {
-                    Log.EscribirLog(ex.Message);                    
-                }
+                EmailBody = sb.ToString();                
                 
                 EnviarEmail(asignacion.Usuario2.Email, asunto, EmailBody);
 

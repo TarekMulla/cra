13efeec329bdf5ec124c74a9217b99e5f673b8ea
paulbeltran <paulbeltran@c3772aea-f7fb-1ae5-96b0-6ad07730f538>
2013-10-10 01:30:48 +0000

    update usuario 2

--- AlterTableInserts/Brasil/Script valores brasil.sql
+++ AlterTableInserts/Brasil/Script valores brasil.sql
@@ -57,3 +57,45 @@ Insert into PAPERLESS_PASOS_USUARIO1_V2 values (7,'Inserir terminal de redestina
 Insert into PAPERLESS_PASOS_USUARIO1_V2 values (8,'Disponibilizar invoice para financeiro (Intranet)',1,7,9,2,null)
 Insert into PAPERLESS_PASOS_USUARIO1_V2 values (9,'Alimentar planilha de controle de embarque',1,8,10,2,null)
 Insert into PAPERLESS_PASOS_USUARIO1_V2 values (10,'Alimentar planilha de erros',1,9,null,2,null)
+
+
+update PAPERLESS_PASOS_USUARIO1_v2 set pantalla  = 'EnviarAvisoUsuario2' where IdPaso=30
+update PAPERLESS_PASOS_USUARIO1_v2 set pantalla  = 'IngresoDeDatos' where IdPaso=21
+
+
+insert into PAPERLESS_PASOS_USUARIO2 values (1,'Conferencia',1,null,2,2, null)
+insert into PAPERLESS_PASOS_USUARIO2 values (2,'Exceções Entry',1,1,3,2,'RegistrarExcepciones')
+insert into PAPERLESS_PASOS_USUARIO2 values (3,'Vinculacion',1,2,null,2,'PresentarManifiesto')
+
+update PAPERLESS_TIPO_CARGA set Descripcion = 'LCL/RO' where ID= 2
+
+insert into PAPERLESS_TIPO_CARGA values ('LCL/FH',1)
+
+insert into PAPERLESS_PASOS_USUARIO2 values (1,'Conferencia',1,null,2,4, null)
+insert into PAPERLESS_PASOS_USUARIO2 values (2,'Exceções Entry',1,1,3,4,'RegistrarExcepciones')
+insert into PAPERLESS_PASOS_USUARIO2 values (3,'Vinculacion',1,2,null,4,'PresentarManifiesto')
+
+
+Insert into PAPERLESS_PASOS_USUARIO1_V2 values (1,'Imprimir docs e tracking',1,null,2,4,'IngresoDeDatos')
+Insert into PAPERLESS_PASOS_USUARIO1_V2 values (2,'Conferir docs',1,1,3,4,null)
+Insert into PAPERLESS_PASOS_USUARIO1_V2 values (3,'Gerar consolidada',1,2,4,4,null)
+Insert into PAPERLESS_PASOS_USUARIO1_V2 values (4,'Enviar HBL ao cliente',1,3,5,4,null)
+Insert into PAPERLESS_PASOS_USUARIO1_V2 values (5,'Redestinar (Santos)',1,4,6,4,null)
+Insert into PAPERLESS_PASOS_USUARIO1_V2 values (6,'Aprovar docs para agente',1,5,7,4,null)
+Insert into PAPERLESS_PASOS_USUARIO1_V2 values (7,'Inserir courier number na consolidada',1,6,8,4,null)
+Insert into PAPERLESS_PASOS_USUARIO1_V2 values (8,'Inserir free time na consolidada',1,7,9,4,'EnviarAvisoUsuario2')
+
+
+alter PROCEDURE SP_L_PAPERLESS_TIPO_CARGA  
+@Activo bit  
+  
+AS  
+  
+SELECT   
+Id,  
+Descripcion,  
+Activo  
+FROM PAPERLESS_TIPO_CARGA  
+WHERE Activo = @Activo  
+Order by Descripcion
+  
